8f9ebf3e953a01ed972809b64de62bf43c16e6f9
Watch650 <famicanxi4life@gmail.com>
2024-10-10 10:30:15 +0700

    Partial progress on LandingPage and Navbar

---
+++ lmsproject/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,22 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="first-page">
+        <div className="first-page-container">
+          <h1>Joy of learning & teaching...</h1>
+          <div className="first-page-container-description">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              odio augue, congue vitae interdum sed, rutrum et elit. Integer
+              tempus hendrerit leo sit amet mattis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
---
+++ lmsproject/components/Navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+import "./navbar.css";
+// import category from "";
+
+function Navbar() {
+  return (
+    <>
+      <header className="header">
+        <div className="top-half">
+          <div className="top-left">
+            <select id="currency">
+              <option value="USD">USD ($)</option>
+              <option value="VND">VND (₫)</option>
+              <option value="EUR">EUR (€)</option>
+            </select>
+
+            <select id="language">
+              <option value="US">🇺🇸 English</option>
+              <option value="VN">🇻🇳 Vietnamese</option>
+              <option value="FR">🇫🇷 French</option>
+            </select>
+
+            <div className="search-container">
+              <form action="">
+                <input type="text" placeholder="Search.." name="search" />
+                <button type="submit">
+                  <i className="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+            </div>
+          </div>
+
+          <div className="top-right"></div>
+        </div>
+
+        <hr />
+
+        <div className="low-half">
+          <a href="/" className="logo">
+            LOGO
+          </a>
+
+          <div className="menu">
+            <button className="categories-button">Categories</button>
+            {/* <a href="">Categories</a> */}
+
+            <nav className="navbar">
+              <a href="">Home</a>
+              <a href="">Courses</a>
+              <a href="">Instructors</a>
+            </nav>
+          </div>
+
+          <button className="learn-button">Start Learning</button>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
