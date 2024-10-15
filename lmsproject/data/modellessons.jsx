e86e8266b3c051f91fca4755a8e1e1eeab924e81
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-10-15 16:41:01 +0700

    feat: update general CourseContent page layout

--- lmsproject/data/modellessons.jsx
+++ lmsproject/data/modellessons.jsx
@@ -1,25 +1,29 @@
 const modellessons = [
     {
-        title: "Quiz 1: Something",
-        time: 10
-    },
-    {
-        title: "Quiz 1: Something",
-        time: 10
-    },
-    {
-        title: "Quiz 1: Something",
-        time: 10
-    },
-    {
-        title: "Quiz 1: Something",
-        time: 10
-    },
-    {
-        title: "Quiz 1: Something",
-        time: 10
+        title: "Lesson 1: Something",
+        time: 30
     },
 
+    {
+        title: "Lesson 1: Something",
+        time: 30
+    },
+    {
+        title: "Lesson 1: Something",
+        time: 30
+    },
+    {
+        title: "Lesson 1: Something",
+        time: 30
+    },
+    {
+        title: "Lesson 1: Something",
+        time: 30
+    },
+    {
+        title: "Lesson 1: Something",
+        time: 30
+    },
 ]
 
 export default modellessons
--- lmsproject/data/modelquizzes.jsx
+++ lmsproject/data/modelquizzes.jsx
@@ -1,29 +1,25 @@
 const modelquizzes = [
     {
-        title: "Lesson 1: Something",
-        time: 30
+        title: "Quiz 1: Something",
+        time: 10
+    },
+    {
+        title: "Quiz 1: Something",
+        time: 10
+    },
+    {
+        title: "Quiz 1: Something",
+        time: 10
+    },
+    {
+        title: "Quiz 1: Something",
+        time: 10
+    },
+    {
+        title: "Quiz 1: Something",
+        time: 10
     },
 
-    {
-        title: "Lesson 1: Something",
-        time: 30
-    },
-    {
-        title: "Lesson 1: Something",
-        time: 30
-    },
-    {
-        title: "Lesson 1: Something",
-        time: 30
-    },
-    {
-        title: "Lesson 1: Something",
-        time: 30
-    },
-    {
-        title: "Lesson 1: Something",
-        time: 30
-    },
 ]
 
 export default modelquizzes
